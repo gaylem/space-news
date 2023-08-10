2483aaec478647146266393d5100607d5a6883df
Gayle Martin <hello.gmartin@gmail.com>
2023-08-10 17:00:24 -0500

    final changes i promise

--- main.js
+++ main.js
@@ -28,3 +28,36 @@ document.addEventListener('DOMContentLoaded', () => {
       date.innerText = today.toLocaleString('en-US', options);
     });
 });
+
+// Async Await
+
+// document.addEventListener('DOMContentLoaded', async () => {
+//   let img = document.getElementById('photo');
+//   let caption = document.getElementById('caption');
+//   let title = document.getElementById('title');
+//   let date = document.getElementById('date');
+//   let copyright = document.getElementById('copyright');
+
+//   try {
+//     const response = await fetch(url);
+//     const data = await response.json();
+
+//     console.log(data);
+//     img.src = data.hdurl;
+//     caption.innerText = data.explanation;
+//     title.innerText = data.title;
+//     if (data.copyright === undefined) copyright.style.display = 'none';
+//     else copyright.innerText = data.copyright;
+//     const today = new Date();
+
+//     const options = {
+//       weekday: 'long',
+//       month: 'long',
+//       year: 'numeric',
+//       day: 'numeric',
+//     };
+//     date.innerText = today.toLocaleString('en-US', options);
+//   } catch (error) {
+//     console.error(Error);
+//   }
+// });
