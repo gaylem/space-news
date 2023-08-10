439e3e4f084275c11582f77415b29a6893cb25a8
Gayle Martin <hello.gmartin@gmail.com>
2023-08-09 23:00:20 -0500

    I'm tired goodnight

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-const apiKey = "4LI6zdP9bsTQ8EtctAniacB3nVF7t2fceEa3SIgF";
+const apiKey = '4LI6zdP9bsTQ8EtctAniacB3nVF7t2fceEa3SIgF';
 const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', () => {
   let img = document.getElementById('photo');
   let caption = document.getElementById('caption');
-  let title = document.getElementById("title");
-  let date = document.getElementById("date");
-  
+  let title = document.getElementById('title');
+  let date = document.getElementById('date');
+  let copyright = document.getElementById('copyright');
+
   fetch(url)
-    .then((data) => data.json())
-    .then((data) => {
+    .then(data => data.json())
+    .then(data => {
+      console.log(data);
       img.src = data.hdurl;
       caption.innerText = data.explanation;
       title.innerText = data.title;
+      copyright.innerText = data.copyright;
       date.innerText = data.date;
     });
-})
+});
