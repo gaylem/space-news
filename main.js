9955c7b9cffffa66d018fd0a62e880f42e328650
Gayle Martin <hello.gmartin@gmail.com>
2023-08-09 21:33:29 -0500

    Pushing progress

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-var contents = document.getElementById('contents');
-var contentsParent = contents.parentNode;
-contentsParent.removeChild(contents);
-contentsParent.prepend(
-  <img src='https://media.wired.com/photos/593261cab8eb31692072f129/master/w_2560%2Cc_limit/85120553.jpg' />,
-);
+const apiKey = "4LI6zdP9bsTQ8EtctAniacB3nVF7t2fceEa3SIgF";
+const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
+
+document.addEventListener("DOMContentLoaded", () => {
+  let img = document.getElementById('photo');
+  let caption = document.getElementById('caption');
+  let title = document.getElementById("title");
+  let date = document.getElementById("date");
+  
+  fetch(url)
+    .then((data) => data.json())
+    .then((data) => {
+      img.src = data.hdurl;
+      caption.innerText = data.explanation;
+      title.innerText = data.title;
+      date.innerText = data.date;
+    });
+})
